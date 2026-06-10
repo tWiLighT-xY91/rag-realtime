d7a78c49fb0ae71a5e2eba24715ce930ec1b137d
tWiLighT-xY91 <shamitharaj318@gmail.com>
2026-06-10 19:39:52 +0530

    Fixed CORS issue while public deployment

--- backend/main.py
+++ backend/main.py
@@ -22,8 +22,7 @@ Base.metadata.create_all(bind=engine)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5173",
-        "http://localhost:5173",
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
